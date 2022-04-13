2ee567fd952551e2ee13dc655ee3f3810994cb54
Chris Mackey <chris@ladybug.tools>
2022-04-13 12:37:01 -0400

    fix(deps): Bump pollination-honeybee-radiance

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     setup_requires=['setuptools_scm'],
     url='https://github.com/pollination/imageless-annual-glare',                   # will be translated to home
     project_urls={
-        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/annualrecipe.png',
+        'icon': 'https://raw.githubusercontent.com/ladybug-tools/artwork/master/icons_components/honeybee/png/annualglare.png',
         'docker': 'https://hub.docker.com/r/ladybugtools/honeybee-radiance'
     },
     description='Imageless annual glare recipe for Pollination.',                  # will be used as package description
