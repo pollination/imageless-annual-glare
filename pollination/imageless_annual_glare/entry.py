fc9346367ff365f35fd66e44ef800552242aab9e
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-11-10 15:05:48 +0100

    fix(entry): Group several tasks in GroupedDAG

--- pollination/imageless_annual_glare/entry.py
+++ pollination/imageless_annual_glare/entry.py
@@ -1,12 +1,7 @@
 from pollination_dsl.dag import Inputs, DAG, task, Outputs
 from dataclasses import dataclass
-from pollination.honeybee_radiance.sun import CreateSunMatrix, ParseSunUpHours
-from pollination.honeybee_radiance.translate import CreateRadianceFolderGrid
-from pollination.honeybee_radiance.octree import CreateOctree, CreateOctreeWithSky
-from pollination.honeybee_radiance.sky import CreateSkyDome, CreateSkyMatrix
-from pollination.honeybee_radiance.grid import SplitGridFolder, MergeFolderData
+from pollination.honeybee_radiance.grid import MergeFolderData
 from pollination.honeybee_radiance.post_process import AnnualGlareAutonomy
-from pollination.path.copy import Copy
 
 # input/output alias
 from pollination.alias.inputs.model import hbjson_model_grid_input
@@ -19,6 +14,7 @@ from pollination.alias.inputs.schedule import schedule_csv_input
 from pollination.alias.outputs.daylight import glare_autonomy_results
 
 
+from ._prepare_folder import ImagelessAnnualGlarePrepareFolder
 from ._raytracing import ImagelessAnnualGlare
 
 
@@ -107,145 +103,71 @@ class ImagelessAnnualGlareEntryPoint(DAG):
         spec={'type': 'number'}
     )
 
-    @task(template=CreateRadianceFolderGrid)
-    def create_rad_folder(self, input_model=model, grid_filter=grid_filter):
-        """Translate the input model to a radiance folder."""
+    @task(template=ImagelessAnnualGlarePrepareFolder)
+    def prepare_folder_imageless_annual_glare(
+        self, north=north, cpu_count=cpu_count,
+        min_sensor_count=min_sensor_count,  grid_filter=grid_filter,
+        model=model, wea=wea
+    ):
         return [
             {
-                'from': CreateRadianceFolderGrid()._outputs.model_folder,
+                'from': ImagelessAnnualGlarePrepareFolder()._outputs.model_folder,
                 'to': 'model'
             },
             {
-                'from': CreateRadianceFolderGrid()._outputs.bsdf_folder,
-                'to': 'model/bsdf'
+                'from': ImagelessAnnualGlarePrepareFolder()._outputs.resources,
+                'to': 'resources'
             },
             {
-                'from': CreateRadianceFolderGrid()._outputs.sensor_grids_file,
-                'to': 'results/grids_info.json'
+                'from': ImagelessAnnualGlarePrepareFolder()._outputs.results,
+                'to': 'results'
             },
             {
-                'from': CreateRadianceFolderGrid()._outputs.sensor_grids,
-                'description': 'Sensor grids information.'
-            }
-        ]
-
-    @task(template=Copy, needs=[create_rad_folder])
-    def copy_grid_info(self, src=create_rad_folder._outputs.sensor_grids_file):
-        return [
-            {
-                'from': Copy()._outputs.dst,
-                'to': 'metrics/ga/grids_info.json'
-            }
-        ]
-
-    @task(template=CreateOctree, needs=[create_rad_folder])
-    def create_octree(self, model=create_rad_folder._outputs.model_folder):
-        """Create octree from radiance folder."""
-        return [
-            {
-                'from': CreateOctreeWithSky()._outputs.scene_file,
-                'to': 'resources/scene.oct'
-            }
-        ]
-
-    @task(
-        template=SplitGridFolder, needs=[create_rad_folder],
-        sub_paths={'input_folder': 'grid'}
-    )
-    def split_grid_folder(
-        self, input_folder=create_rad_folder._outputs.model_folder,
-        cpu_count=cpu_count, cpus_per_grid=1, min_sensor_count=min_sensor_count
-    ):
-        """Split sensor grid folder based on the number of CPUs"""
-        return [
-            {
-                'from': SplitGridFolder()._outputs.output_folder,
-                'to': 'resources/grid'
+                'from': ImagelessAnnualGlarePrepareFolder()._outputs.initial_results,
+                'to': 'initial_results'
             },
             {
-                'from': SplitGridFolder()._outputs.dist_info,
-                'to': 'initial_results/ga/_redist_info.json'
-            },
-            {
-                'from': SplitGridFolder()._outputs.sensor_grids,
-                'description': 'Sensor grids information.'
-            }
-        ]
-
-    @task(template=Copy, needs=[split_grid_folder])
-    def copy_redist_info(self, src=split_grid_folder._outputs.dist_info):
-        return [
-            {
-                'from': Copy()._outputs.dst,
-                'to': 'initial_results/dgp/_redist_info.json'
-            }
-        ]
-
-    @task(template=CreateSkyDome)
-    def create_sky_dome(self):
-        """Create sky dome for daylight coefficient studies."""
-        return [
-            {'from': CreateSkyDome()._outputs.sky_dome, 'to': 'resources/sky.dome'}
-        ]
-
-    @task(template=CreateSkyMatrix)
-    def create_total_sky(self, north=north, wea=wea, sun_up_hours='sun-up-hours'):
-        return [
-            {'from': CreateSkyMatrix()._outputs.sky_matrix,
-             'to': 'resources/sky.mtx'}
-        ]
-
-    @task(template=CreateSunMatrix)
-    def generate_sunpath(self, north=north, wea=wea):
-        """Create sunpath for sun-up-hours."""
-        return [
-            {
-                'from': CreateSunMatrix()._outputs.sun_modifiers,
-                'to': 'resources/suns.mod'
-            }
-        ]
-
-    @task(template=ParseSunUpHours, needs=[generate_sunpath])
-    def parse_sun_up_hours(self, sun_modifiers=generate_sunpath._outputs.sun_modifiers):
-        return [
-            {
-                'from': ParseSunUpHours()._outputs.sun_up_hours,
-                'to': 'results/sun-up-hours.txt'
+                'from': ImagelessAnnualGlarePrepareFolder()._outputs.sensor_grids
             }
         ]
 
     @task(
         template=ImagelessAnnualGlare,
-        needs=[
-            create_sky_dome, create_octree, create_total_sky, create_rad_folder,
-            split_grid_folder
-        ],
-        loop=split_grid_folder._outputs.sensor_grids,
+        needs=[prepare_folder_imageless_annual_glare],
+        loop=prepare_folder_imageless_annual_glare._outputs.sensor_grids,
         # create a subfolder for each grid
         sub_folder='initial_results/{{item.full_id}}',
         # sensor_grid sub_path
-        sub_paths={'sensor_grid': '{{item.full_id}}.pts'}
+        sub_paths={
+            'octree_file': 'scene.oct',
+            'sensor_grid': 'grid/{{item.full_id}}.pts',
+            'sky_matrix': 'sky.mtx',
+            'sky_dome': 'sky.dome',
+            'bsdfs': 'bsdf'
+            }
     )
     def annual_imageless_glare(
         self,
         radiance_parameters=radiance_parameters,
-        octree_file=create_octree._outputs.scene_file,
+        octree_file=prepare_folder_imageless_annual_glare._outputs.resources,
         grid_name='{{item.full_id}}',
-        sensor_grid=split_grid_folder._outputs.output_folder,
+        sensor_grid=prepare_folder_imageless_annual_glare._outputs.resources,
         sensor_count='{{item.count}}',
-        sky_matrix=create_total_sky._outputs.sky_matrix,
-        sky_dome=create_sky_dome._outputs.sky_dome,
-        bsdfs=create_rad_folder._outputs.bsdf_folder,
+        sky_matrix=prepare_folder_imageless_annual_glare._outputs.resources,
+        sky_dome=prepare_folder_imageless_annual_glare._outputs.resources,
+        bsdfs=prepare_folder_imageless_annual_glare._outputs.model_folder,
         luminance_factor=luminance_factor
     ):
         pass
 
     @task(
         template=MergeFolderData,
-        needs=[annual_imageless_glare]
+        needs=[prepare_folder_imageless_annual_glare, annual_imageless_glare],
+        sub_paths={'input_folder': 'dgp'}
     )
     def restructure_daylight_glare_probability_results(
-        self, input_folder='initial_results/dgp', extension='dgp'
+        self, input_folder=prepare_folder_imageless_annual_glare._outputs.initial_results,
+        extension='dgp'
     ):
         return [
             {
